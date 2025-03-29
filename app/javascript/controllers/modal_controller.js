1dc632f7e94cf1369fc847a223cc5c8a21186d42
Olga Grabek <olga.grabek@gmail.com>
2025-03-29 15:55:42 +0100

    Add the modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = ["dialog"];
+
+  initialize() {
+    this.modal = this.element;
+    this.element.addEventListener("turbo:frame-load", () => {
+      this.open();
+    });
+  }
+
+  open() {
+    this.dialogTarget.showModal();
+  }
+
+  close() {
+    this.dialogTarget.close();
+    this.element.removeAttribute("src");
+    this.element.innerHTML = "";
+  }
+}
