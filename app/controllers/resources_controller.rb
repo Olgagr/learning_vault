1846f819c1ba78037d096280b085fe0c8c6309e2
Olga Grabek <olga.grabek@gmail.com>
2025-04-02 14:26:21 +0200

    Edit a resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -12,13 +12,30 @@ class ResourcesController < ApplicationController
     @resource = Current.user.resources.new(resource_params)
 
     if @resource.save
-      @resource.categories << Current.user.categories.where(id: params[:resource][:category_ids])
       respond_to do |format|
         format.turbo_stream { flash.now[:notice] = "Resource created successfully" }
         format.html { redirect_to resources_path, notice: "Resource created successfully" }
       end
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @resource = Current.user.resources.find(params[:id])
+    @categories = Current.user.categories
+  end
+
+  def update
+    @resource = Current.user.resources.find(params[:id])
+
+    if @resource.update(resource_params)
+      respond_to do |format|
+        format.turbo_stream { flash.now[:notice] = "Resource updated successfully" }
+        format.html { redirect_to resources_path, notice: "Resource updated successfully" }
+      end
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -31,7 +48,8 @@ class ResourcesController < ApplicationController
       :resource_type,
       :progress_status,
       :link,
-      :owned
+      :owned,
+      category_ids: []
     )
   end
 end
